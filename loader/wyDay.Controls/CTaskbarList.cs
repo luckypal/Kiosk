1c2694b6c0f2e6bd7aa3500f6db9e41c6ac7e596
luckypal <bigluckypal@gmail.com>
2019-12-01 05:37:03 +0900

    Fix issues from Loader project

--- loader/wyDay.Controls/CTaskbarList.cs
+++ loader/wyDay.Controls/CTaskbarList.cs
@@ -8,7 +8,7 @@ namespace wyDay.Controls
 	[Guid("56FDF344-FD6D-11d0-958A-006097C9A090")]
 	internal class CTaskbarList
 	{
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		public extern CTaskbarList();
+		//[MethodImpl(MethodImplOptions.InternalCall)]
+		//public extern CTaskbarList();
 	}
 }
--- loader/wyUpdate/ShellLink.cs
+++ loader/wyUpdate/ShellLink.cs
@@ -7,7 +7,7 @@ namespace wyUpdate
 	[Guid("00021401-0000-0000-C000-000000000046")]
 	public class ShellLink
 	{
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		public extern ShellLink();
+		//[MethodImpl(MethodImplOptions.InternalCall)]
+		//public extern ShellLink();
 	}
 }
